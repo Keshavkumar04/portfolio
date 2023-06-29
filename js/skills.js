039a1e8d3c91477f33391ab8db5ed96b3f58933b
Keshavkumar04 <keshavkumar2003.1march@gmail.com>
2023-06-29 16:12:49 +0530

    updating links

--- js/skills.js
+++ js/skills.js
@@ -1,17 +1,17 @@
-// document.addEventListener('scroll', function() {
-//     var scrollEffect = document.querySelector('.scroll-effect');
-//     var skillSection = document.querySelector('#skill-section');
-//     var scrollPosition = window.scrollY;
+document.addEventListener('scroll', function() {
+    var scrollEffect = document.querySelector('.scroll-effect');
+    var skillSection = document.querySelector('#skill-section');
+    var scrollPosition = window.scrollY;
   
-//     var skillSectionOffsetTop = skillSection.offsetTop;
-//     var skillSectionHeight = skillSection.offsetHeight;
-//     var skillSectionThreshold = skillSectionOffsetTop + (0.37 * skillSectionHeight);
-//     var scrollThreshold = skillSectionOffsetTop - 230;
+    var skillSectionOffsetTop = skillSection.offsetTop;
+    var skillSectionHeight = skillSection.offsetHeight;
+    var skillSectionThreshold = skillSectionOffsetTop + (0.37 * skillSectionHeight);
+    var scrollThreshold = skillSectionOffsetTop - 230;
   
-//     if ((scrollPosition >= scrollThreshold && scrollPosition <= skillSectionOffsetTop) || (scrollPosition >= skillSectionOffsetTop && scrollPosition <= skillSectionThreshold)) {
-//       scrollEffect.style.left = '50px';
-//     } else {
-//       scrollEffect.style.left = '-200px';
-//     }
-//   });
+    if ((scrollPosition >= scrollThreshold && scrollPosition <= skillSectionOffsetTop) || (scrollPosition >= skillSectionOffsetTop && scrollPosition <= skillSectionThreshold)) {
+      scrollEffect.style.left = '50px';
+    } else {
+      scrollEffect.style.left = '-200px';
+    }
+  });
   
